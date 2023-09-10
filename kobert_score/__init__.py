47620d3d895c477d375521cc5d6e10089daf5c3a
jacques.ai <jacques.ai@tunib.ai>
2023-09-10 23:41:25 +0900

    0.0.1

--- kobert_score/__init__.py
+++ kobert_score/__init__.py
@@ -1,2 +1,5 @@
-__version__ = "0.0.1"
-from .score import score, compare_similarity
+# Copyright (C) 2023 Sungyoon Lee <syoon369@gmail.com>
+# All rights reserved.
+
+from .score import score
+from .compare_similarity import compare_similarity
